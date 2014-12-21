3e3ad1bf596fee585612b51fb400df302c7c6f93
tamaki <testplayers@gmail.com>
2014-12-22 07:50:40 +0900

    add site table and csv file for site

--- 2ch_matome/create_table.rb
+++ 2ch_matome/create_table.rb
@@ -3,10 +3,19 @@ require 'active_record'
 ActiveRecord::Base.establish_connection(YAML.load_file('database.yml'))
 
 ActiveRecord::Migration.create_table :items do |t|
+  t.string :site_id, null: false, default: false
   t.string :title, null: false, default: false
   t.string :link, null: false, default: false
   t.datetime :dc_date, null: false, default: false
   t.string :image_link
+  t.references :site_id, index: true, null: false
+
+  t.timestamps
+end
+
+ActiveRecord::Migration.create_table :sites do |t|
+  t.string :name, null: false, default: false
+  t.string :url, null: false, default: false
 
   t.timestamps
 end
